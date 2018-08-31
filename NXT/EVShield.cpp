42ea652105a2da8fd306053b244a6a50f4d06524
Arjan Schoorl <arjanschoorl@gmail.com>
2018-08-31 13:14:48 +0200

    Update EVShield.cpp
    With the new libraries included

--- NXT/EVShield.cpp
+++ NXT/EVShield.cpp
@@ -37,12 +37,29 @@
   static void pingEV();
 #endif
 
-
 #if defined(__AVR__)
 	static void callbackLED();
 #elif defined(__PIC32MX__)
 	uint32_t callbackLED(uint32_t);
 #endif
+
+#if defined(ARDUINO_ARC32_TOOLS)
+  #include "CurieTimerOne.h"
+#else
+  #include "MsTimer2.h"
+#endif
+
+extern "C" {
+#if ( ARDUINO == 10608 )
+#include "../../hardware/arduino/avr/libraries/Wire/src/utility/twi.h"
+#elif ( ARDUINO == 10605 )
+#include "../../hardware/arduino/avr/libraries/Wire/utility/twi.h"
+#elif defined(ESP8266)
+// previously included: ".../hardware/esp8266/2.3.0/cores/esp8266/twi.h"
+#else
+uint8_t twi_writeTo(uint8_t, uint8_t*, uint8_t, uint8_t, uint8_t);
+#endif
+}
 
 byte initCounter = 0;
 bool btnState_go, btnState_left, btnState_right;
@@ -1587,4 +1604,605 @@ uint16_t	EVShieldUART::readValue()
     uint16_t result;
     result = readLocationInt(0x83+m_offset);
     return (result);
+}
+
+// BaseI2CDevice
+// Max I2C message length is 16 bytes.  
+const int BUFFER_LEN = 16;  
+
+
+// Initialize static variables
+uint8_t* BaseI2CDevice::_buffer = 0;
+bool BaseI2CDevice::b_initialized = false;
+
+
+BaseI2CDevice::BaseI2CDevice(uint8_t i2c_address)
+{
+  // As I understand it, an I2C bus can address 127 different devices (a 7-bit quantity).
+  // When used, the 7-bit quantity is shifted right one bit, and the last bit is clear
+  // for a read operation and set for a write operation.  Arduino's Wire library expects
+  // a 7-bit address, but most tech specs list the 8-bit address.  Hence, we drop
+  // the least significant bit (and Wire.h shifts the address and sets the read/write
+  // bit as appropriate.)
+  b_initialized = false;
+  _device_address = i2c_address >> 1;
+
+  _buffer = (uint8_t*) calloc(BUFFER_LEN, sizeof(uint8_t));
+}
+
+void BaseI2CDevice::initProtocol()
+{
+  if ( b_initialized ) return;
+  #if defined(ESP8266)
+  Wire.begin(D2,D3);
+  #else
+  Wire.begin();
+  #endif
+  b_initialized = true;
+}
+
+// READING FUNCTIONS
+
+// Reads registers of an I2C device.
+// See the documentation for your device to know what a given register
+// or register range indicates.
+uint8_t* BaseI2CDevice::readRegisters(
+  uint8_t  start_register,   // start of the register range
+  uint8_t  bytes_to_read,   // number of bytes to read (max 16 for lego devices)
+  uint8_t* buffer,      // (optional) user-supplied buffer
+  uint8_t  buffer_length,    // (optional) length of user-supplied buffer
+  bool     clear_buffer)    // should we zero out the buffer first? (optional)
+{
+  #if defined(ARDUINO_ARC32_TOOLS)
+    CurieTimerOne.rdRstTickCount();
+  #else
+    MsTimer2::reset();
+  #endif
+  if (!buffer)
+  {
+    buffer = _buffer;
+  }
+
+  if (!buffer_length)
+  {
+    buffer_length = BUFFER_LEN;
+  }
+
+  bytes_to_read = min(bytes_to_read, buffer_length);  // avoid buffer overflow
+
+  if (clear_buffer)
+  {
+    memset(buffer, 0, buffer_length);
+  }
+
+  // We write to the I2C device to tell it where we want to read from
+  Wire.beginTransmission(_device_address);
+#if defined(ARDUINO) && ARDUINO >= 100
+    Wire.write(start_register);
+#else
+    Wire.send(start_register);
+#endif
+    //Wire.send(bytes_to_read);
+    Wire.endTransmission();
+
+    // Now we can read the data from the device
+  Wire.requestFrom(_device_address, bytes_to_read);
+
+    for (uint8_t index = 0; Wire.available(); ++index)
+    {
+#if defined(ARDUINO) && ARDUINO >= 100
+      buffer[index] = Wire.read();
+#else
+      buffer[index] = Wire.receive();
+#endif
+    }
+
+    _write_error_code = Wire.endTransmission();
+        
+  #if defined(ARDUINO_ARC32_TOOLS)
+    CurieTimerOne.rdRstTickCount();
+  #else
+    MsTimer2::reset();
+  #endif
+  return buffer;
+}
+
+// Reads a byte from the given register on the I2C device.
+uint8_t BaseI2CDevice::readByte(uint8_t location)
+{
+  readRegisters(location, 1);
+  return _buffer[0];
+}
+
+// Reads two bytes from the given register pair on the I2C device.
+int16_t BaseI2CDevice::readInteger(uint8_t location)
+{
+  readRegisters(location, 2);
+
+  // I believe the data has the least significant byte first
+  return readIntFromBuffer(_buffer);
+}
+
+// Reads four bytes from the given registers, starting at the specified location, on the I2C device.
+uint32_t BaseI2CDevice::readLong(uint8_t location)
+{
+  readRegisters(location, 4);
+  return readLongFromBuffer(_buffer);
+}
+
+// Reads a string.  Be certain that your buffer is large enough
+// to hold the string and a trailing 'nul'!
+char* BaseI2CDevice::readString(
+    uint8_t  location,       // starting location of the string
+    uint8_t  bytes_to_read,   // number of bytes to read
+    uint8_t* buffer,      // optional user-supplied buffer
+    uint8_t  buffer_length)    // length of user-supplied buffer)
+{
+  return (char *)readRegisters(location, bytes_to_read, buffer, buffer_length, true);
+}
+
+
+// WRITING FUNCTIONS
+
+// Returns true if the write was successful.
+// If not true, you may check the result by calling getWriteErrorCode.
+bool BaseI2CDevice::writeRegisters(
+  uint8_t  start_register,   // start of the register range
+  uint8_t  bytes_to_write,   // number of bytes to write
+  uint8_t* buffer)    // optional user-supplied buffer
+{
+  #if defined(ARDUINO_ARC32_TOOLS)
+    CurieTimerOne.rdRstTickCount();
+  #else
+    MsTimer2::reset();
+  #endif
+  if (!buffer)
+  {
+    buffer = _buffer;
+  }
+
+  // We write to the I2C device to tell it where we want to read from and how many bytes
+  Wire.beginTransmission(_device_address);
+#if defined(ARDUINO) && ARDUINO >= 100
+  Wire.write(start_register);
+#else
+  Wire.send(start_register);
+#endif
+
+  // Send the data
+  for (uint8_t index = 0; index < bytes_to_write; ++index)
+  {
+#if defined(ARDUINO) && ARDUINO >= 100
+    Wire.write(buffer[index]);
+#else
+    Wire.send(buffer[index]);
+#endif
+  }
+
+  _write_error_code = Wire.endTransmission();
+
+  #if defined(ARDUINO_ARC32_TOOLS)
+    CurieTimerOne.rdRstTickCount();
+  #else
+    MsTimer2::reset();
+  #endif
+  return _write_error_code == 0;  // 0 indicates success
+}
+
+// Writes a byte to a given register of the I2C device
+bool BaseI2CDevice::writeByte(uint8_t location, uint8_t data)
+{
+  return writeRegisters(location, 1, &data);
+}
+
+// Writes two bytes to a given register of the I2C device
+bool BaseI2CDevice::writeInteger(uint8_t location, uint16_t data)
+{
+  writeIntToBuffer(_buffer, data);
+  return writeRegisters(location, 2, _buffer);
+}
+
+// Writes four bytes to a given register of the I2C device
+bool BaseI2CDevice::writeLong(uint8_t location, uint32_t data)
+{
+  writeLongToBuffer(_buffer, data);
+  return writeRegisters(location, 4, _buffer);
+}
+
+// This is the status value returned from the last write command.
+// A return value of zero indicates success.
+// Non-zero results indicate failures.  From libraries/Wire/utility/twi.c, they are:
+//          1 .. length to long for buffer
+//          2 .. address send, NACK received
+//          3 .. data send, NACK received
+//          4 .. other twi error (lost bus arbitration, bus error, ..)
+uint8_t BaseI2CDevice::getWriteErrorCode()
+{
+  return _write_error_code;
+}
+
+// READ SOME INFORMATION OFF OF THE DEVICE
+// returns a string with the current firmware version of the device
+char* BaseI2CDevice::getFirmwareVersion()
+{
+  return readString(0, 8);
+}
+
+// returns a string indicating the vendor of the device
+char* BaseI2CDevice::getVendorID()
+{
+  return readString(0x08, 8);
+}
+
+// returns a string indicating the device's ID
+char* BaseI2CDevice::getDeviceID()
+{
+  return readString(0x10, 8);
+}
+
+/** returns a string indicating the features on this device
+ some devices may return null.
+*/
+char* BaseI2CDevice::getFeatureSet()
+{
+  return readString(0x18, 8);
+}
+
+// It is very unusual to do this
+void BaseI2CDevice::setWriteErrorCode(uint8_t code)
+{
+  _write_error_code = code;
+}
+
+
+/**
+ * checkAddress()
+ * this function checks to see if there is 
+ * any device at its specified address 
+ */
+
+bool BaseI2CDevice::checkAddress()
+{
+  uint8_t *txBuffer;
+  int8_t x = 1;
+#if defined(__PIC32MX__)
+  x = twi_writeTo(_device_address, txBuffer, 0, 1) == 0;
+#else
+  #if (defined(ARDUINO) && ARDUINO <= 100) || defined(ESP8266)
+    x = twi_writeTo(_device_address, txBuffer, 0, 1) == 0;
+  #else
+    x = twi_writeTo(_device_address, txBuffer, 0, 1, 1) == 0;
+  #endif
+#endif
+  return (x != 0);
+}
+
+
+/**
+ * setAddress(address)
+ * this function set's the i2c address
+ * for this instance to given address 
+ * Note that, generally i2c address of a physical device does not change.
+ * Use this function if there are multiple devices on your bus and you want to 
+ * conserve processor memory from instantiating another class instance.
+ */
+
+bool BaseI2CDevice::setAddress(uint8_t i2c_address)
+{
+  _device_address = i2c_address >> 1;
+  return true;
+}
+
+uint8_t BaseI2CDevice::getAddress()
+{
+  return _device_address;
+}
+
+//SoftI2cMaster
+const int BUFF_LEN = 16;
+//------------------------------------------------------------------------------
+// WARNING don't change anything unless you verify the change with a scope
+//------------------------------------------------------------------------------
+// set device address to begin
+SoftI2cMaster::SoftI2cMaster(uint8_t devAddr){
+  initialized = false;
+  deviceAddr = devAddr;
+  _so_buffer = (uint8_t*) calloc(BUFF_LEN, sizeof(uint8_t));
+}
+//------------------------------------------------------------------------------
+// init pins and set bus high
+void SoftI2cMaster::initProtocol(uint8_t sclPin, uint8_t sdaPin){
+  if ( initialized ) return;
+
+  if (!sclPin && !sdaPin){
+   #if defined(__AVR__)
+    #if defined(__AVR_ATmega168__) || defined(__AVR_ATmega8__) || defined(__AVR_ATmega328P__)
+      sclPin_ = 19;   // A5
+      sdaPin_ = 18;   // A4
+    #else
+      sclPin_ = 21;
+      sdaPin_ = 20;
+    #endif
+#elif defined(__PIC32MX__)
+     #if defined(_BOARD_UNO_)
+     	 sclPin_ = 46;   // use SCL pin 
+    	 sdaPin_ = 45;   // use SDA pin 
+     #elif defined(_BOARD_MEGA_)
+     	 sclPin_ = 21;
+       sdaPin_ = 20;
+     #endif
+
+#endif
+  }
+  else {
+    sclPin_ = sclPin;
+    sdaPin_ = sdaPin;
+  }
+  digitalWrite(sclPin_, LOW);
+  digitalWrite(sdaPin_, LOW);
+  pinMode(sclPin_,INPUT);
+  pinMode(sdaPin_,INPUT);
+  initialized = true;
+}
+//------------------------------------------------------------------------------
+// read a byte and send Ack if last is false else Nak to terminate read
+uint8_t SoftI2cMaster::read(uint8_t last){
+  uint8_t b = 0;
+  // make sure pullup enabled
+  pinMode(sdaPin_,INPUT);
+  // read byte
+  for (uint8_t i = 0; i < 8; i++) {
+  // don't change this loop unless you verify the change with a scope
+  b <<= 1;
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sclPin_,INPUT);
+  if (digitalRead(sdaPin_)) b |= 1;
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sclPin_,OUTPUT);
+  }
+  // send Ack or Nak
+  pinMode(sdaPin_, OUTPUT);
+  if (last)  pinMode(sdaPin_, INPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sclPin_,INPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sclPin_,OUTPUT);
+  pinMode(sdaPin_,INPUT);
+  return b;
+}
+//------------------------------------------------------------------------------
+// send new address and read/write with stop
+uint8_t SoftI2cMaster::restart(uint8_t addressRW){
+  uint8_t e;
+  delayMicroseconds(I2C_DELAY_USEC);
+  stop();
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sclPin_,OUTPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sclPin_,INPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+  e = start(addressRW);
+  delayMicroseconds(I2C_DELAY_USEC);
+  return e;
+}
+//------------------------------------------------------------------------------
+// issue a start condition for i2c address with read/write bit
+uint8_t SoftI2cMaster::start(uint8_t addressRW){
+  pinMode(sdaPin_,OUTPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sclPin_,OUTPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+  return write(addressRW);
+}
+//------------------------------------------------------------------------------
+// issue a stop condition
+void SoftI2cMaster::stop(void){
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sclPin_,INPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sdaPin_,INPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+}
+//------------------------------------------------------------------------------
+// write byte and return true for Ack or false for Nak
+uint8_t SoftI2cMaster::write(uint8_t b){
+  // write byte
+  for (uint8_t m = 0X80; m != 0; m >>= 1) {
+    if(m & b) pinMode(sdaPin_, INPUT);
+    else pinMode(sdaPin_, OUTPUT);
+    // don't change this loop unless you verify the change with a scope
+    pinMode(sclPin_,INPUT);
+    delayMicroseconds(I2C_DELAY_USEC);
+    pinMode(sclPin_,OUTPUT);
+    delayMicroseconds(I2C_DELAY_USEC);
+  }
+  // get Ack or Nak
+  pinMode(sdaPin_,INPUT);
+  pinMode(sdaPin_, INPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sclPin_,INPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+  b = digitalRead(sdaPin_);
+  pinMode(sclPin_,OUTPUT);
+  delayMicroseconds(I2C_DELAY_USEC);
+  pinMode(sdaPin_, OUTPUT);
+  if ( b != 0 ) stop ();
+  return b == 0;
+}
+//------------------------------------------------------------------------------
+// read number of bytes from start register and return values; optional buffer
+uint8_t* SoftI2cMaster::readRegisters(uint8_t startRegister, uint8_t bytes, uint8_t* buf){
+  //delay(20);
+  if (!buf)    buf = _so_buffer;
+  bytes = min(bytes,BUFF_LEN);  // avoid buffer overflow
+  memset(buf, 0, BUFF_LEN);
+  _error_code = 0;
+  // issue a start condition, send device address and write direction bit
+  if (!start(deviceAddr | I2C_WRITE)) {
+		_error_code = 2;
+	  return false;
+	}
+  // send the start register address  
+  if (!write(startRegister)) {
+		_error_code = 1;
+	  return false;
+	}
+  // issue a repeated stop and start condition, send device address and read direction bit  
+  if (!restart(deviceAddr | I2C_READ)) {
+		_error_code = 3;
+	  return false;
+	}
+  // read data from the device and store into buffer  
+  for (uint8_t i = 0; i < bytes; i++) {
+    // send Ack until last byte then send Ack
+    buf[i] = read(i == (bytes-1));
+  }
+  // issue a stop condition  
+  stop();
+  return buf;
+}
+//------------------------------------------------------------------------------
+// write number of bytes from buffer, location is included in the buf
+bool SoftI2cMaster::writeRegistersWithLocation(int bytes, uint8_t* buf){
+  if (!start(deviceAddr | I2C_WRITE)) return false;
+  for (int i = 0; i < bytes; i++){
+    if (!write(buf[i]))  return false;
+  }
+  stop();
+  return true;
+}
+
+//------------------------------------------------------------------------------
+// write number of bytes from buffer at specified location.
+bool SoftI2cMaster::writeRegisters  (uint8_t location, uint8_t bytes_to_write,
+              uint8_t* buffer)
+{
+  if (!buffer) buffer = _so_buffer;
+
+  uint8_t buf[BUFF_LEN];
+  memset(buf, 0, BUFF_LEN);
+  buf[0] = location;
+  memmove(buf+1, buffer, bytes_to_write);
+  return writeRegistersWithLocation(bytes_to_write+1, buf);
+}
+
+//------------------------------------------------------------------------------
+// write a single byte at speccified location.
+bool SoftI2cMaster::writeByte  (uint8_t location, uint8_t data)
+{
+  return writeRegisters(location, 1, &data);
+}
+
+//------------------------------------------------------------------------------
+// write one integer (2 bytes) at speccified location.
+bool SoftI2cMaster::writeInteger(uint8_t location, uint16_t data)
+{
+  uint8_t buf[2];
+  buf[0] = data & 0xFF;
+  buf[1] = (data >> 8) & 0xFF;
+  return writeRegisters(location, 2, buf);
+}
+
+//------------------------------------------------------------------------------
+// write one long (4 bytes) at speccified location.
+bool SoftI2cMaster::writeLong  (uint8_t location, uint32_t data)
+{
+  uint8_t buf[4];
+
+  buf[0] = data & 0xFF;
+  buf[1] = (data >>  8) & 0xFF;
+  buf[2] = (data >> 16) & 0xFF;
+  buf[3] = (data >> 24) & 0xFF;
+  return writeRegisters(location, 4, buf);
+}
+
+//------------------------------------------------------------------------------
+// read string: convert read registers to character string
+char* SoftI2cMaster::readString(uint8_t startRegister, uint8_t bytes,
+                    uint8_t* buf, uint8_t len)
+{
+	char *x;
+	x = (char *) readRegisters(startRegister, bytes, buf);
+	if ( x == false ) {
+	  return (char *) "";
+	}
+  return (char *) x;
+}
+//------------------------------------------------------------------------------
+// read one byte, total of 8 bits
+uint8_t SoftI2cMaster::readByte (uint8_t location){
+  if ( readRegisters(location, 1) != false ) {
+		return _so_buffer[0];
+  } else {
+	  return 0;
+	}
+}
+//------------------------------------------------------------------------------
+// read integer, total of 2 bytes or 16 bits
+int16_t SoftI2cMaster::readInteger (uint8_t location){
+
+  if ( readRegisters(location, 2) != false ) {
+		return _so_buffer[0] | (_so_buffer[1] << 8);
+  } else {
+	  return 0;
+	}
+}
+//------------------------------------------------------------------------------
+// read long integer, total of 4 bytes or 32 bits
+uint32_t SoftI2cMaster::readLong (uint8_t location){
+
+  if ( readRegisters(location, 4) != false ) {
+		return (uint32_t)_so_buffer[0] |
+               (((uint32_t)_so_buffer[1]) << 8) |
+               (((uint32_t)_so_buffer[2]) << 16) |
+               (((uint32_t)_so_buffer[3]) << 24);
+  } else {
+	  return 0;
+	}
+}
+
+// This is the status value returned from the last write command.
+// A return value of zero indicates success.
+// Non-zero results indicate failures.  From libraries/Wire/utility/twi.c, they are:
+//                1: write fail
+//                2: start fail
+//                3: restart fail
+uint8_t SoftI2cMaster::getWriteErrorCode()
+{
+  return _error_code;
+}
+
+//------------------------------------------------------------------------------
+// get firmware version
+char* SoftI2cMaster::getFirmwareVersion(){
+  return readString(0x00, 8);
+}
+//------------------------------------------------------------------------------
+// get vendor ID
+char* SoftI2cMaster::getVendorID(){  
+  return readString(0x08, 8);
+}
+//------------------------------------------------------------------------------
+// get device ID
+char* SoftI2cMaster::getDeviceID(){
+  return readString(0x10, 8);
+}
+/**
+ * this function checks to see if there is 
+ * any device at its specified address 
+ */
+bool SoftI2cMaster::checkAddress()
+{
+// FIXME: This function is not working correctly.
+  uint8_t *txBuffer;
+  int8_t x = 1;
+	if (!start(deviceAddr | I2C_WRITE)) return false;
+	stop();
+	return true;
+}
+
+bool SoftI2cMaster::setAddress(uint8_t address)
+{
+  deviceAddr = address;
+  return true;
 }
